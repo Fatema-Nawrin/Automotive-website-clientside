8deac18621b62886dc30ec3c9d0b76b00a6e8538
Fatema-Nawrin <nawrin4000@gmail.com>
2022-05-25 17:28:58 +0600

    make admin functtion added

--- src/Pages/Dashboard/User.js
+++ src/Pages/Dashboard/User.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const User = ({ user, index, refetch }) => {
+    const { email, role } = user;
+
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/users/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if (res.status === 403) {
+                    toast.error('Sorry, failed to make this user admin.')
+                }
+                return res.json();
+            })
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    refetch();
+                    toast('Succesfully given a user admin role.')
+                }
+            })
+    }
     return (
 
         <tr>
             <th>{index + 1}</th>
             <td>{user.email}</td>
-            <td><button className='btn btn-primary btn-sm'>Make Admin</button></td>
+            <td>{role === 'admin' ? <p className='text-secondary font-semibold'>Admin</p> : <button onClick={makeAdmin} className='btn btn-primary btn-sm'>Make Admin</button>}</td>
         </tr>
 
 
